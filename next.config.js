7441e199abf708c0ce93f647898faf65e57d6813
Alexander Worthington <ppg@Alexanders-MacBook-Air.local>
2026-03-29 18:13:46 -0400

    Revert "Add Sanity CMS: Studio at /studio, content schemas, base configuration"
    This reverts commit a656e163e892d71b2a4e7be6ae6b3dcf06a149f0.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 module.exports = nextConfig;
--- schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import article from './article'
-import page from './page'
-import siteSettings from './siteSettings'
-
-export const schemaTypes = [article, page, siteSettings]
